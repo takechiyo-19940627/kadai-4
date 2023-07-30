31448f08065fe10c6fabf519e5cb7237a2cf908b
takechiyo-19940627 <take.19940627@gmail.com>
2023-07-30 04:35:35 +0000

    test: add test cases

---
+++ test_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo === テストを実行します ===
+
+echo 引数が1つもないケース ===
+
+./calculate_gretest_common_measure.sh
+echo
+
+echo === 引数が自然数かつ1つだけ渡されている場合 ===
+./calculate_gretest_common_measure.sh 1
+echo
+
+echo === 引数が自然数かつ3つ渡されている場合 ===
+./calculate_gretest_common_measure.sh 1 2 3
+echo
+
+echo === 引数が自然数であるが、コンマで区切っている場合 ===
+./calculate_gretest_common_measure.sh 1,000 100
+echo
+
+echo === 引数が文字列の場合 ===
+./calculate_gretest_common_measure.sh 数字 数字
+echo
+
+echo === 引数が負の整数の場合 ===
+./calculate_gretest_common_measure.sh -10 -2
+echo
+
+echo === 引数が小数の場合 ===
+./calculate_gretest_common_measure.sh 0.4 0.8
+echo
+
+echo === 入力された2つの自然数にのうちいずれかがBigIntの場合 ===
+./calculate_gretest_common_measure.sh 21474836470000 2147483647
+echo
+
