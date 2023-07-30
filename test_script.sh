fe2b034af5bf985533102099eec5352082183fbd
takechiyo-19940627 <take.19940627@gmail.com>
2023-07-30 07:41:17 +0000

    fix: error check

--- test_script.sh
+++ test_script.sh
@@ -3,68 +3,28 @@
 echo === テストを実行します ===
 
 echo ===引数が1つもないケース ===
-./calculate_gretest_common_measure.sh
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi
-echo
+./calculate_gretest_common_measure.sh 1 2 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が自然数かつ1つだけ渡されている場合 ===
-./calculate_gretest_common_measure.sh 1 2
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit
-fi
-echo
+./calculate_gretest_common_measure.sh 1  && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が自然数かつ3つ渡されている場合 ===
-./calculate_gretest_common_measure.sh 1 2 3
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi 
-echo
+./calculate_gretest_common_measure.sh 1 2 3 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が自然数であるが、コンマで区切っている場合 ===
-./calculate_gretest_common_measure.sh 1,000 100
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi 
-echo
+./calculate_gretest_common_measure.sh 1,000 100 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が文字列の場合 ===
-./calculate_gretest_common_measure.sh 数字 数字
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi
-echo
+./calculate_gretest_common_measure.sh 数字 数字 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が負の整数の場合 ===
-./calculate_gretest_common_measure.sh -10 -2
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi
-echo
+./calculate_gretest_common_measure.sh -10 -2 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 引数が小数の場合 ===
-./calculate_gretest_common_measure.sh 0.4 0.8
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi
-echo
+./calculate_gretest_common_measure.sh 0.4 0.8 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 入力された2つの自然数にのうちいずれかがBigIntの場合 ===
-./calculate_gretest_common_measure.sh 21474836470000 2147483647
-if [ "$?" = 0 ]; then
-  echo 'unexpected error' >&2
-  exit 1
-fi
-echo
+./calculate_gretest_common_measure.sh 21474836470000 2147483647 && echo "error: this case must be fails" >&2 && exit 1
 
 echo === 成功するケース ===
 result=$(./calculate_gretest_common_measure.sh 24 32)
@@ -77,6 +37,5 @@ if [ $result != 8 ]; then
   exit 1
 fi
 echo "最大公約数: $result"
-echo
 
 echo === テストが完了しました ===
